725a7716b09a5d528cc8c916d6bee2724ab9a9c2
TBSniller <51515147+TBSniller@users.noreply.github.com>
2021-05-16 00:24:26 +0200

    Create script.js

---
+++ piccap/js/script.js
@@ -0,0 +1,53 @@
+function displayReponse()
+{
+    var tmp = document.getElementById("input").value;
+    var request = webOS.service.request("luna://com.tbsniller.piccap.service/", {
+        method:"start",
+        parameters: {name: tmp},    
+        onFailure: showFailure,
+        onComplete: showResponse,
+    //  subscribe = false,
+    //  resubscribe = false
+    });
+}
+ 
+function getLog()
+{
+    var tmp = document.getElementById("input").value;
+    var request = webOS.service.request("luna://com.tbsniller.piccap.service/", {
+        method:"log",
+        parameters: {name: tmp},    
+        onFailure: showFailure,
+        onComplete: showResponse,
+    //  subscribe = false,
+    //  resubscribe = false
+    });
+}
+
+function stop()
+{
+    var tmp = document.getElementById("input").value;
+    var request = webOS.service.request("luna://com.tbsniller.piccap.service/", {
+        method:"stop",
+        parameters: {name: tmp},    
+        onFailure: showFailure,
+        onComplete: showResponse,
+    //  subscribe = false,
+    //  resubscribe = false
+    });
+}
+ 
+function showResponse(inResponse)   {
+    if(inResponse.returnValue) {
+        document.getElementById("result1").innerHTML += inResponse.data;     
+        document.getElementById("result2").innerHTML = "Service Responded";     
+    }
+    else {
+        document.getElementById("result1").innerHTML += "Failed!";
+    }
+    return true;
+}
+ 
+function showFailure(inError){
+    document.getElementById("result1").innerHTML += "Failed!";
+}
